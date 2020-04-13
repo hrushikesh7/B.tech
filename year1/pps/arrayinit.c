b0f6eb3623a3820953f3a712c23426e013f57eb4
Guna <hrushikeshkanukuntla@gmai.com>
2020-04-14 01:54:39 +0530

    arrayinit

--- year1/pps/arrayinit.c
+++ year1/pps/arrayinit.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-   int data[5], i;
+   int data[5], i;            //data[i]=a[i]
    printf("Enter elements: ");
    for(i = 0; i < 5; ++i)
-     scanf("%d",data + i);
+     scanf("%d",data + i);     //data+i = &a[i]
    printf("You entered: \n");
    for(i = 0; i < 5; ++i)
-      printf("%d\n", *(data + i));
+      printf("%d\n", *(data + i));       //*(data+i) = a[i]
    return 0;
 }
