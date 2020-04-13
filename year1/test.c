2bbba3c2d5a6be081e8a0504a8b065efe6b553dc
Guna <hrushikeshkanukuntla@gmai.com>
2020-04-14 02:04:00 +0530

    some

---
+++ year1/test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    char name[i];
+    printf(" enter names");
+    for(i=0;i<5;i++)
+    {
+        scanf("%c",&name[i]);
+    }
+    printf("names: "name[i]);
+}
